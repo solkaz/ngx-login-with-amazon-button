2f579511be7923f070c62bc896ee0341691d4c82
Jeff Held <jh.software.developer@gmail.com>
2019-09-05 14:30:55 +0200

    Implement inputs for height/width/image src for button

--- projects/ngx-login-with-amazon-button/src/lib/ngx-login-with-amazon-button.component.spec.ts
+++ projects/ngx-login-with-amazon-button/src/lib/ngx-login-with-amazon-button.component.spec.ts
@@ -1,25 +1,90 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgxLoginWithAmazonButtonComponent } from './ngx-login-with-amazon-button.component';
+import { Component } from '@angular/core';
 
 describe('NgxLoginWithAmazonButtonComponent', () => {
-  let component: NgxLoginWithAmazonButtonComponent;
-  let fixture: ComponentFixture<NgxLoginWithAmazonButtonComponent>;
+  describe('Standalone tests', () => {
+    let component: NgxLoginWithAmazonButtonComponent;
+    let fixture: ComponentFixture<NgxLoginWithAmazonButtonComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NgxLoginWithAmazonButtonComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [NgxLoginWithAmazonButtonComponent],
+      }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgxLoginWithAmazonButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(() => {
+      fixture = TestBed.createComponent(NgxLoginWithAmazonButtonComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('has a default width and height for the button', () => {
+      const button = (fixture.nativeElement as HTMLElement)
+        .querySelector('a')
+        .querySelector('img');
+      expect(button.width).toBe(156);
+      expect(button.height).toBe(32);
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Integration tests', () => {
+    const src = 'https://via.placeholder.com/50';
+    @Component({
+      template: `
+        <lwa-button [width]="width" [height]="height" [src]="src"> </lwa-button>
+      `,
+    })
+    class TestHostComponent {
+      width = 50;
+      height = 50;
+      src = src;
+    }
+
+    let component: TestHostComponent;
+    let fixture: ComponentFixture<TestHostComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [NgxLoginWithAmazonButtonComponent, TestHostComponent],
+      }).compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TestHostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('has an <a> element with a `LoginWithAmazon` id', () => {
+      expect((fixture.nativeElement as HTMLElement).querySelector('a').id).toBe(
+        'LoginWithAmazon'
+      );
+    });
+
+    it('accepts width and height inputs', () => {
+      const button = (fixture.nativeElement as HTMLElement)
+        .querySelector('a')
+        .querySelector('img');
+      expect(button.width).toBe(50);
+      expect(button.height).toBe(50);
+      component.height = 100;
+      component.width = 100;
+      fixture.detectChanges();
+      expect(button.width).toBe(100);
+      expect(button.height).toBe(100);
+    });
+
+    it('accepts image src input', () => {
+      const button = (fixture.nativeElement as HTMLElement)
+        .querySelector('a')
+        .querySelector('img');
+      expect(button.src).toEqual(src);
+      const newSrc = 'https://via.placeholder.com/149';
+      button.src = newSrc;
+      fixture.detectChanges();
+      expect(button.src).toEqual(newSrc);
+    });
   });
 });
--- projects/ngx-login-with-amazon-button/src/lib/ngx-login-with-amazon-button.component.ts
+++ projects/ngx-login-with-amazon-button/src/lib/ngx-login-with-amazon-button.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-NgxLoginWithAmazonButton',
+  selector: 'lwa-button',
   template: `
-    <p>
-      ngx-login-with-amazon-button works!
-    </p>
+    <a href id="LoginWithAmazon">
+      <img
+        [src]="src"
+        [width]="width"
+        [height]="height"
+        alt="Login with Amazon"
+        border="0"
+      />
+    </a>
   `,
-  styles: []
+  styles: [],
 })
 export class NgxLoginWithAmazonButtonComponent implements OnInit {
+  @Input() height = 32;
+  @Input() width = 156;
+  /**
+   * Possible choices for this can be found on the
+   * [Login With Amazon docs](https://developer.amazon.com/docs/login-with-amazon/button.html#website-images)
+   */
+  @Input() src: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
