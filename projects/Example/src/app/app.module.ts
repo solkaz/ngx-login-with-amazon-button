d83236c3f5b64d426358d8068629936f78c5cafb
Jeff Held <jh.software.developer@gmail.com>
2019-10-08 12:45:36 +0200

    Initial setup displaying a LWA button

--- projects/Example/src/app/app.module.ts
+++ projects/Example/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+  LWA_CLIENT_ID,
+  LWA_SDK_OBJECT,
+  NgxLoginWithAmazonButtonModule,
+} from 'ngx-login-with-amazon-button';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxLoginWithAmazonButtonModule],
+  providers: [
+    {
+      provide: LWA_SDK_OBJECT,
+      useValue: {
+        authorize(options, next) {
+          console.log('authorizing');
+          console.log('options:', options);
+          if (typeof next !== 'string') {
+            next();
+          } else {
+            console.log('routing to', next);
+          }
+        },
+        setClientId(id: string) {
+          console.log('set client id!', id);
+        },
+      },
+    },
+    {
+      provide: LWA_CLIENT_ID,
+      useValue: 'testing',
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
