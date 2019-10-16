bcbfa519944466030949300812e4abb81a48a2e0
Jeff Held <jh.software.developer@gmail.com>
2019-10-16 11:34:15 +0200

    Authorize, retrieveProfile, and logout functionality

--- projects/Example/src/app/app.component.ts
+++ projects/Example/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxLoginWithAmazonButtonService } from 'ngx-login-with-amazon-button';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  onAuthorize = () => {
-    console.log('authorized!');
+  profile?: UserProfile;
+  constructor(private lwaSdk: NgxLoginWithAmazonButtonService) {}
+  onAuthorize = (response: AccessTokenRequest) => {
+    this.lwaSdk.lwaSdk.retrieveProfile(response.access_token, (res) => {
+      if (res.success === false) {
+        throw new Error('Couldn\'t retrieve profile: ' + res.error);
+      }
+      this.profile = res.profile;
+    });
+  }
+
+  logout = () => {
+    this.lwaSdk.lwaSdk.logout();
+    this.profile = undefined;
   }
 }
