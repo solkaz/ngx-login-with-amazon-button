3d89d7f60cc557b463fac71069919d7e419637ab
Jeff Held <jh.software.developer@gmail.com>
2019-10-18 15:38:40 +0200

    Integration test for module

--- projects/ngx-login-with-amazon-button/src/lib/ngx-login-with-amazon-button.module.spec.ts
+++ projects/ngx-login-with-amazon-button/src/lib/ngx-login-with-amazon-button.module.spec.ts
@@ -1,5 +1,8 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { LWA_CLIENT_ID, LWA_SDK_OBJECT } from './injection-tokens';
+import { NgxLoginWithAmazonButtonComponent } from './ngx-login-with-amazon-button.component';
 import { NgxLoginWithAmazonButtonModule } from './ngx-login-with-amazon-button.module';
 import { lwaSdkMock } from './test-injection-tokens';
 
@@ -29,5 +32,34 @@ describe('NgxLoginWithAmazonButtonModule', () => {
       ],
     });
     expect(TestBed.get(LWA_CLIENT_ID)).toEqual('testId');
+  });
+
+  it('exports NgxLoginWithAmazonButtonComponent', () => {
+    @Component({
+      template: `
+        <lwa-button></lwa-button>
+      `,
+    })
+    class TestHostComponent {}
+    return TestBed.configureTestingModule({
+      imports: [NgxLoginWithAmazonButtonModule.forRoot('testId')],
+      providers: [
+        {
+          provide: LWA_SDK_OBJECT,
+          useValue: lwaSdkMock,
+        },
+      ],
+      declarations: [TestHostComponent],
+    })
+      .compileComponents()
+      .then(() => {
+        const fixture = TestBed.createComponent(TestHostComponent);
+        fixture.detectChanges();
+        expect(
+          fixture.debugElement.query(
+            By.directive(NgxLoginWithAmazonButtonComponent)
+          )
+        ).toBeDefined();
+      });
   });
 });
