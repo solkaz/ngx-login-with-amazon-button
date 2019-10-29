bb5d475d3e8e3bc41527d705f87ccfcc041c60f0
Jeff Held <jh.software.developer@gmail.com>
2019-10-29 12:23:46 +0100

    Doc building on Netlify

---
+++ build-docs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if ! [ `command -v compodoc` ]; then
+    yarn global add @compodoc/compodoc
+fi
+
+cd projects/ngx-login-with-amazon-button
+compodoc -p ./tsconfig.lib.json -d ../../documentation
+cd ../..
