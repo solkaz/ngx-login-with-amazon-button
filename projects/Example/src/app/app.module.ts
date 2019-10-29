68d15654c693d5466f72bc09523f1b88f0561a5b
Jeff Held <jh.software.developer@gmail.com>
2019-10-29 11:39:57 +0100

    Deal with AOT issue

--- projects/Example/src/app/app.module.ts
+++ projects/Example/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxLoginWithAmazonButtonModule } from 'ngx-login-with-amazon-button';
+import {
+  NgxLoginWithAmazonButtonModule,
+  LWA_CLIENT_ID,
+} from 'ngx-login-with-amazon-button';
 import { AppComponent } from './app.component';
 import { ProfileDisplayComponent } from './profile-display/profile-display.component';
 import { ScopePickerComponent } from './scope-picker/scope-picker.component';
@@ -8,16 +11,22 @@ import { ScopePickerFieldComponent } from './scope-picker-field/scope-picker-fie
 import { environment } from '../environments/environment';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    NgxLoginWithAmazonButtonModule.forRoot(environment.clientId),
+  ],
   declarations: [
     AppComponent,
     ProfileDisplayComponent,
     ScopePickerComponent,
     ScopePickerFieldComponent,
   ],
-  imports: [
-    BrowserModule,
-    NgxLoginWithAmazonButtonModule.forRoot(environment.clientId),
-  ],
+  /**
+   * For now, we need to provide `LWA_CLIENT_ID` due to issue with
+   * AOT compilation and environment logic.
+   * Issue: https://github.com/angular/angular/issues/22829
+   */
+  providers: [{ provide: LWA_CLIENT_ID, useValue: environment.clientId }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
