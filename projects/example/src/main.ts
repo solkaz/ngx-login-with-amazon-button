300c04399e6931a504f579efa84d07fb556625a6
Jeff Held <jh.software.developer@gmail.com>
2019-10-29 16:50:02 +0100

    What if I tried loading the SDK last?

--- projects/example/src/main.ts
+++ projects/example/src/main.ts
@@ -19,4 +19,23 @@ function bootstrapApp() {
     .catch((err) => console.error(err));
 }
 
-globalThis.onAmazonLoginReady = bootstrapApp;
+/**
+ * If the SDK has loaded before the app bundle has loaded, then
+ * `bootstrapApp` will not be called automatically; if so, then
+ * `bootstrapApp` can safely be called manually.
+ *
+ * To check if the SDK was loaded, check if the <script /> tag is present.
+ * It has an `id` of `amazon-login-sdk`, which will cause a reference to
+ * the element to be added as to the global scope.
+ *
+ * See [2ality's post about IDs being global](https://2ality.com/2012/08/ids-are-global.html)
+ */
+if (globalThis['amazon-login-sdk']) {
+  bootstrapApp();
+} else {
+  /**
+   * `onAmazonLoginReady` is to be called when the Login with Amazon SDK
+   * has been loaded.
+   */
+  globalThis.onAmazonLoginReady = bootstrapApp;
+}
