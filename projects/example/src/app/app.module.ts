173e84a58d4a0f05b1a861fe00e17488862a8b5e
Jeff Held <jh.software.developer@gmail.com>
2019-10-30 11:10:55 +0100

    Add header

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -4,24 +4,30 @@ import {
   NgxLoginWithAmazonButtonModule,
   LWA_CLIENT_ID,
 } from 'ngx-login-with-amazon-button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { ProfileDisplayComponent } from './profile-display/profile-display.component';
 import { ScopePickerComponent } from './scope-picker/scope-picker.component';
 import { ScopePickerFieldComponent } from './scope-picker-field/scope-picker-field.component';
 import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    NgxLoginWithAmazonButtonModule.forRoot(environment.clientId),
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    NgxLoginWithAmazonButtonModule.forRoot(environment.clientId),
   ],
   declarations: [
     AppComponent,
     ProfileDisplayComponent,
     ScopePickerComponent,
     ScopePickerFieldComponent,
+    HeaderComponent,
   ],
   /**
    * For now, we need to provide `LWA_CLIENT_ID` due to issue with
