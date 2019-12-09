780e266c8eb1458ca447fb0296d19269415c0159
Jeff Held <jh.software.developer@gmail.com>
2019-12-09 19:28:45 +0100

    feature: add headless chrome test launcher

--- projects/ngx-login-with-amazon-button/karma.conf.js
+++ projects/ngx-login-with-amazon-button/karma.conf.js
@@ -3,32 +3,38 @@
 
 module.exports = function(config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
-      require("@angular-devkit/build-angular/plugins/karma"),
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require("path").join(
+      dir: require('path').join(
         __dirname,
-        "../../coverage/ngx-login-with-amazon-button"
+        '../../coverage/ngx-login-with-amazon-button'
       ),
-      reports: ["html", "lcovonly", "text-summary"],
+      reports: ['json'],
       fixWebpackSourcePaths: true,
     },
-    reporters: ["dots", "kjhtml"],
+    reporters: ['dots', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: [],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
